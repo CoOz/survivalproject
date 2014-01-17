3fd3b99a53feaac5f07061a534aebed1b5c45b09
Adrien SANVOISIN <lecooz@gmail.com>
2014-01-17 03:03:57 +0100

    ajout DigMap.hx et MapWorld.hx

---
+++ source/cdjv/game/survival/DigMap.hx
@@ -0,0 +1,12 @@
+package cdjv.game.survival;
+
+class DigMap{
+
+    public function new(){
+
+    }
+
+    public function creuse(x:Float,y:Float){
+
+    }
+}
---
+++ source/cdjv/game/survival/MapWorld.hx
@@ -0,0 +1,34 @@
+package cdjv.game.survival;
+
+import flixel.FlxG;
+import flixel.util.FlxPool;
+import flixel.tile.FlxTilemap;
+import flixel.FlxSprite;
+
+class MapWorld extends FlxSprite{
+    private var scene:PlayState;
+    private var zones:Map<String,FlxTilemap>;
+    private var tileMapPool:FlxPool<FlxTilemap>;
+
+    public var digMap:DigMap;
+
+    public function new(scene:PlayState){
+        super();
+        this.scene=scene;
+        tileMapPool=new FlxPool<FlxTilemap>();
+        for(nbTM in 0...9)
+            tileMapPool.put(new FlxTilemap());
+
+    }
+    public function loadForCoords(xPos:Float,yPos:Float){
+        if(xPos%FlxG.game.width<10 || xPos%FlxG.game.width>FlxG.game.width-10){ //bords!
+
+        }
+
+        loadDigMap(xPos,yPos);
+    }
+
+    public function loadDigMap(xPos:Float,yPos:Float){
+
+    }
+}
