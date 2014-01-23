959fcfa9187bdbd840d2bb4c218b2feab85151e2
Regnak <vigneau.joel@gmail.com>
2014-01-23 17:31:06 +0100

    test2

--- source/cdjv/game/survival/MapWorld.hx
+++ source/cdjv/game/survival/MapWorld.hx
@@ -10,22 +10,47 @@ class MapWorld extends FlxSprite{
     private var zones:Map<String,FlxTilemap>;
     private var tileMapPool:FlxPool<FlxTilemap>;
     // Loading the map.
-   /* [Embed(source = "data/map.txt", mimeType = "application/octet-stream")] private var txtMap:Class;
+    //[Embed(source = "data/map.txt", mimeType = "application/octet-stream")] private var txtMap:Class;
     // Loading the map spritesheet.
-    [Embed(source = "data/tile.png")] private var imgMap:Class;*/
+    //[Embed(source = "data/tile.png")] private var imgMap:Class;
     // Map variable
     public var map:FlxTilemap;
     public var digMap:DigMap;
+    private var tabMap:Array<Int>;
+
 
     public function new(scene:PlayState){
         super();
+        this.scene = scene;
+        tabMap =    [0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0
+                     0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0
+                     0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0
+                     0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0
+                     0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0
+                     0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0
+                     0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0
+                     0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0
+                     0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0
+                     0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0
+                     0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0
+                     0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0
+                     0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0
+                     0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0
+                     0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0];
+    map = new FlxTilemap();
+    map.widthInTiles = 40;
+    map.heightInTiles = 40;
     // Setting up the map.
-        //array 20c 15l
-   /* map = new FlxTilemap();
-    map.startingIndex = 0;
-    map.drawIndex = 0;
-    map.loadMap(new txtMap, imgMap, 40, 40);*/
-    //add(map);
+    
+    //map.startingIndex = 0;
+    //map.drawIndex = 0;
+    map.loadMap(tabMap, "assets/images/tile.png", 40, 40);
+    map.updateFrameData();
+
+    this.scene.add(map);
+//add(map);
+
+
         /*this.scene=scene;
         tileMapPool=new FlxPool<FlxTilemap>();
         for(nbTM in 0...9)
