c591afab1613aa4cb10cb5f9115aab8e6161f0d1
Regnak <vigneau.joel@gmail.com>
2014-02-21 22:40:06 +0100

    lastversionNextmerge

--- source/cdjv/game/survival/MapWorld.hx
+++ source/cdjv/game/survival/MapWorld.hx
@@ -20,14 +20,19 @@ class MapWorld extends FlxSprite{
     public var groupMap:FlxTypedGroup<FlxTilemap>;
     public var digMap:DigMap;
     private var tabMap:Array<Int>;
+    private var groupObj:FlxTypedGroup<FlxSprite>;
+    private var obj:FlxSprite;
+    private var bob:Int;
+    //pour le premier passage dans generateMap
+    public var a:Int;
     //private var tabMapAdjacent:Array<Int>;
 
 
     public function new(scene:PlayState){
         super();
         this.scene = scene;
-       // tabMapAdjacent = []
-        tabMap =    [2,2,1,1,1,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,
+       
+        tabMap = [2,2,1,1,1,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,
                      2,2,1,1,1,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,
                      2,2,1,1,1,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,
                      2,2,1,1,1,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,
@@ -42,39 +47,22 @@ class MapWorld extends FlxSprite{
                      2,2,1,1,1,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,
                      2,2,1,1,1,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,
                      2,2,1,1,1,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2];
-    //dans une fonction qui prend la zone en param
-                     
-    groupMap = new FlxTypedGroup(9);
-    for(j in -1...2)
-        for(i in -1...2)
-        {
-            map2 = new FlxTilemap();
-            map2.widthInTiles = 20; 
-            //number of tiles by column
-            map2.heightInTiles = 15;
-            map2.x = j * 800;
-            map2.y = i * 600;
-            map2.loadMap(tabMap, "assets/images/tile.png", 40, 40);
-            map2.updateFrameData();
-            groupMap.add(map2);
-
-        }
     
-    this.scene.add(groupMap);//add(map);
-
-
-       
-
+    a = 0;
+    groupMap = new FlxTypedGroup(9);
+    
     }
     public function generateMap(zoneN:Array<Int> ){
-        groupMap.clear();
+        if(a == 1)
+            groupMap.clear();
+        else a = 1;
         //groupMap = new FlxTypedGroup(9);
         trace((zoneN[0]),zoneN[1]);
         for(j in (zoneN[0]-1)...(zoneN[0]+2))
             for(i in (zoneN[1]-1)...(zoneN[1]+2))
             {
                 map2 = new FlxTilemap();
-                map2.widthInTiles = 20; 
+                map2.widthInTiles = 20;
                 //number of tiles by column
                 map2.heightInTiles = 15;
                 map2.x = j * 800;
@@ -84,8 +72,22 @@ class MapWorld extends FlxSprite{
                 groupMap.add(map2);
             }
         this.scene.add(groupMap);
-               
+
     }
+    //générer le graphic
+    /*public function popAleaObject(zoneN:Array<Int>){
+        var i,j:int;
+        i = Math.random()*20;
+        for(j in 0...j){
+            obj = new FlxSprite();
+            obj.x = Std.int(Math.random()*(800*(zoneN[0]+1)));
+            obj.y = Std.int(Math.random()*(600*(zoneN[1]+1)));
+            groupObj.add(obj);
+        }
+
+        this.scene.add(groupObj);
+   }*/
+
     public function loadForCoords(xPos:Float,yPos:Float){
         if(xPos%FlxG.game.width<10 || xPos%FlxG.game.width>FlxG.game.width-10){ //bords!
 
