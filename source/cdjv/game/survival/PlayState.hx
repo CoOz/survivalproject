985e2dd7b20a5f079fcccdbb56a6d16fde0ffbba
Regnak <vigneau.joel@gmail.com>
2014-02-21 22:00:25 +0100

    Coucou

--- source/cdjv/game/survival/PlayState.hx
+++ source/cdjv/game/survival/PlayState.hx
@@ -13,9 +13,6 @@ import flixel.util.FlxMath;
 class PlayState extends FlxState
 {
 	public var surface:MapWorld;
-	public var zoneN:Array<Int>;
-	public var zoneP:String;
-	private var perso:Character;
 
 	override public function create():Void
 	{
@@ -27,32 +24,35 @@ class PlayState extends FlxState
 		#end
 
         //on crée un monde
-       	surface=new MapWorld(this);
+<<<<<<< HEAD
+       	surface=new MapWorld(this); 
+       	//modifier le param suivant la dernière position du personnage
+       	surface.generateMap([0,0]);
        	//surface.setPosition(0,0);
         //this.add(surface);*/
+=======
+        surface=new MapWorld(this);
+        surface.setPosition(0,0);
+        this.add(surface);
+>>>>>>> origin/Zed
 
         //on crée un perso
-        perso = new Character(this);
-        
+        var perso:Character=new Character(this);
         perso.setPosition(50,50);
         this.add(perso);
-        zoneP = perso.checkZone().toString();
-		trace(zoneP);
 
         surface.loadForCoords(perso.x,perso.y);
-		
+
         FlxG.camera.target=perso;
 
 		
 		super.create();
 	}
 	
-	
 	/**
 	 * Function that is called when this state is destroyed - you might want to 
 	 * consider setting all objects this state uses to null to help garbage collection.
 	 */
-
 	override public function destroy():Void
 	{
 		super.destroy();
@@ -63,12 +63,6 @@ class PlayState extends FlxState
 	 */
 	override public function update():Void
 	{
-		zoneN = perso.checkZone();
-		if((zoneN.toString()) != zoneP){
-			//appele generateMap avec zoneN
-			surface.generateMap(zoneN);
-			zoneP = zoneN.toString();
-		} 
 		super.update();
 	}	
 }
